ed36dca9921901051c1bdd70d7474c444865579d
Vojtech Rinik <vojto@rinik.net>
2014-09-12 14:38:20 +0200

    in with the new

---
+++ Punkt/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Punkt
+//
+//  Created by Vojtech Rinik on 9/12/14.
+//  Copyright (c) 2014 Vojtech Rinik. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
